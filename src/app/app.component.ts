7e25113749b1c9404c58e997339a40182fbe7b56
Ismael <ismagarmon3@gmail.com>
2023-12-21 01:24:39 +0100

    upda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'Task_Manager_Angular';
+
+  ngOnChanges(): void {
+    console.log(this.user)
+  }
+
+  private prueba: string = ""
+
+  public user: string = ""
+
+  public changepropierties(ul: HTMLUListElement): void {
+    this.prueba = "ASDAS"
+    console.log(this.prueba)
+  }
 }
