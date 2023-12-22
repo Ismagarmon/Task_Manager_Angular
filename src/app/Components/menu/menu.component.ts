13c87bfdb6497070ba94f01424a3cd93753b80bd
Ismael <ismagarmon3@gmail.com>
2023-12-22 19:35:06 +0100

    upda

---
+++ src/app/Components/menu/menu.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent implements OnChanges, OnInit {
+
+  ngOnInit(): void {
+    let jsonls = localStorage.getItem("User")
+    this.user
+  }
+
+  ngOnChanges(): void {
+    console.log(this.user)
+  }
+
+  public isLogged: boolean = false
+
+  private prueba: string = ""
+
+  public user: string = ""
+
+  public changepropierties(ul: HTMLUListElement): void {
+    this.prueba = "ASDAS"
+    console.log(this.prueba)
+  }
+
+  public logout(): void {
+    this.isLogged = false
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,17 @@
-import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './Components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnChanges {
+export class AppComponent {
   title = 'Task_Manager_Angular';
 
-  ngOnChanges(): void {
-    console.log(this.user)
-  }
-
-  public isLogged: boolean = false
-
-  private prueba: string = ""
-
-  public user: string = ""
-
-  public changepropierties(ul: HTMLUListElement): void {
-    this.prueba = "ASDAS"
-    console.log(this.prueba)
-  }
-
-  public logout(): void {
-    this.isLogged = false
-  }
+  
 }
