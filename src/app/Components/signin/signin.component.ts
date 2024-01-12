d23c2b27cef07a249e311654ee0e6cc2435fd965
Ismael <ismagarmon3@gmail.com>
2024-01-12 17:42:15 +0100

    up

--- src/app/Components/signin/signin.component.ts
+++ src/app/Components/signin/signin.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Router } from '@angular/router'
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
+import { ValidacionesInputs } from '../../shared/class/validaciones-inputs'
+import { LoginService } from '../../shared/service/apirest.service'
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
 
-  constructor(private router:Router){
+  constructor(private router:Router,private fb: FormBuilder, private login: LoginService){
 
   }
 
-  
+  public formularioLogIn = this.fb.group({
+    email: new FormControl('', [Validators.required, ValidacionesInputs.regexemail]),
+    password: new FormControl('', [Validators.required, ValidacionesInputs.regexpassword]), 
+  })
+
+  public entrar(email: string, password: string): void {
+    let email_value = email
+    let pw_value = password
+  }
 }
