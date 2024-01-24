1ec03716c62db13cb7f31c6f524a314f97e7e883
Ismael <ismagarmon3@gmail.com>
2024-01-24 21:37:57 +0100

    up

---
+++ src/app/Components/musicplayer/reproductor/reproductor.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reproductor',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './reproductor.component.html',
+  styleUrl: './reproductor.component.css'
+})
+export class ReproductorComponent {
+
+}
